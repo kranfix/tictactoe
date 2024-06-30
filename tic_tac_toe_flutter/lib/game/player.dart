f54a8843669d9fc01f078f350e2fb077f7ccc029
Frank Moreno @kranfix <frankmoreno1993@gmail.com>
2024-06-30 12:32:56 -0500

    Flutter: RemotePlayer

--- tic_tac_toe_flutter/lib/game/player.dart
+++ tic_tac_toe_flutter/lib/game/player.dart
@@ -3,19 +3,20 @@ import 'dart:async';
 import './board.dart';
 
 abstract interface class Player {
-  const Player();
-  Future<int> requestNext(BoardSerialization board);
+  const Player({required this.myToken});
+  final Token myToken;
+
+  Future<int> requestNext(BoardSerialization data);
 
   void dispose();
 }
 
-class LocalPlayer implements Player {
+class LocalPlayer extends Player {
   LocalPlayer({
-    required this.myToken,
+    required super.myToken,
   })  : _completer = null,
-        board = Board();
-
-  final Token myToken;
+        board = Board(),
+        super();
 
   Completer<int>? _completer;
   Board board;
@@ -39,5 +40,47 @@ class LocalPlayer implements Player {
     board = Board.deserialize(data);
     _completer ??= Completer();
     return _completer!.future;
+  }
+}
+
+/// In DB we must save
+/// currentBoard: "ox_xoo__x"
+/// lastPlayer: "o", // 'o' | 'x'
+/// lastIndex: 4, // 0..<9
+abstract class RemotePlayer extends Player {
+  RemotePlayer({required super.myToken}) {
+    _unsub = subscribe(_onNext);
+  }
+
+  Completer<int>? _completer;
+
+  // Save BoardSerialization, last player(myToken)
+  Future<void> updateState(BoardSerialization board);
+
+  void Function()? _unsub;
+
+  void Function() subscribe(
+      void Function(Token lastPlayer, int lastIndex) onNext);
+
+  void _onNext(Token lastPlayer, int lastIndex) {
+    // Ignored because it was the local player
+    if (lastPlayer != myToken) return;
+
+    _completer?.complete(lastIndex);
+    _completer = null;
+  }
+
+  @override
+  Future<int> requestNext(BoardSerialization data) async {
+    await updateState(data);
+    _completer ??= Completer();
+    return _completer!.future;
+  }
+
+  @override
+  void dispose() {
+    _unsub?.call();
+    _unsub = null;
+    _completer = null;
   }
 }
